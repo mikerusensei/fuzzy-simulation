421fca32f37313f5e1a59149748a28d67eea59bf
Michael Ponce <michaelalexisponce15@gmail.com>
2024-09-17 15:38:51 +0800

    :wrench: Fixing Parameters

--- prototype.py
+++ prototype.py
@@ -1,24 +1,46 @@
-from module import Fuzzificate, Rule, Interference, Defuzzificate
+from refactor import Fuzzificate, Rule, Interference, Defuzzificate
 
 if __name__ == '__main__':
-    temp = Fuzzificate()
+    fuzz = Fuzzificate()
 
-    temp.add_fuzz("Cold Temp", [20, 25])
-    temp.add_fuzz("Low Temp", [25, 30])
-    temp.add_fuzz("Optimal", [30, 38])
-    temp.add_fuzz("High Temp", [38, 42])
-    temp.add_fuzz("Critical Temp", [42, 45])
+    # temp.add_fuzz("Cold Temp", [26, 26, 32])
+    # temp.add_fuzz("Low Temp", [26, 32, 38])
+    # temp.add_fuzz("Optimal", [32, 38, 44])
+    # temp.add_fuzz("High Temp", [38, 44, 50])
+    # temp.add_fuzz("Critical Temp", [44, 44, 50])
+
+    # temp.add_fuzz("Cold Temp", [26, 32])
+    # temp.add_fuzz("Low Temp", [26, 38])
+    # temp.add_fuzz("Optimal", [32, 44])
+    # temp.add_fuzz("High Temp", [38, 50])
+    # temp.add_fuzz("Critical Temp", [44, 50])
+
+    # fuzz.add_fuzz("Cold Temp", [16, 16, 30])
+    # fuzz.add_fuzz("Low Temp", [16, 25, 34])
+    # fuzz.add_fuzz("Optimal Temp", [30, 34, 38])
+    # fuzz.add_fuzz("High Temp", [34, 42, 50])
+    # fuzz.add_fuzz("Critical Temp", [38, 50, 50])
+
+    fuzz.add_fuzz("Cold Temp", [20, 20, 30])
+    fuzz.add_fuzz("Low Temp", [20, 25, 30])
+    fuzz.add_fuzz("Optimal Temp", [30, 35, 40])
+    fuzz.add_fuzz("High Temp", [40, 45, 50])
+    fuzz.add_fuzz("Critical Temp", [40, 50, 50])
+
 
     rules = Rule()
 
-    rules.add_rule("Cold Temp", "Increase Heater Temp")
-    rules.add_rule("Low Temp", "Turn Heater On")
-    rules.add_rule("Optimal", "Turn off devices")
-    rules.add_rule("High Temp", "Turn Aircondition On")
-    rules.add_rule("Critical Temp", "Increase Aircondition Temp")
+    rules.add_rule("Cold Temp", ["Increase Heater Temp", 2])
+    rules.add_rule("Low Temp", ["Turn Heater On", 1])
+    rules.add_rule("Optimal", ["Turn off Devices", 0])
+    rules.add_rule("High Temp", ["Turn Aircondition On", -1])
+    rules.add_rule("Critical Temp", ["Increase Aircondition Temp", -2])
 
-    interference = Interference(temp, rules)
+    interference = Interference(fuzz, rules)
 
-    temperature = 26
-    crisp = interference.application(temperature)
+    temperature = 30
+    fuzzy_value = interference.application(temperature)
+    control = Defuzzificate(rules, fuzzy_value)
+    crisp = control.defuzzificate()
     print(crisp)
+    
